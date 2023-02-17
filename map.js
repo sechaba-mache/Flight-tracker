5f354ed1628b3d39069cfd59c97ae9da194d6e25
Sechaba Mache <93662067+sechaba-mache@users.noreply.github.com>
2023-02-17 15:56:06 +0200

    feat: :sparkles: Add loadMap, fly, close and addMarker functions

--- map.js
+++ map.js
@@ -1,16 +1,41 @@
+function loadMap() {
+	const map = L.map("my-map").setView([51.505, -0.09], 13);
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+	L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+		maxZoom: 19,
+		attribution:
+			'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	}).addTo(map);
 
-var icon = new L.Icon.Default();
+	map.on("click", close);
+	return map;
+}
 
-var plane = L.icon({
-    iconUrl: 'plane-solid.svg',
-    iconSize: [30, 50]
-});
+function fly(map, lat, long) {
+	addMarker(map, lat, long);
 
-L.marker([51.5, -0.09], {icon: plane}).addTo(map);
+	map.invalidateSize();
+	map.flyTo([lat, long], 13);
+}
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+function addMarker(map, lat, long) {
+	const plane = L.icon({
+		iconUrl: "plane-solid.svg",
+		iconSize: [30, 50],
+	});
+
+	L.marker([lat, long], { icon: plane }).addTo(map);
+}
+
+//move to dom manip
+function close() {
+	const myMap = document.querySelector("#my-map");
+	myMap.style.display = "none";
+
+	const boxes = document.getElementsByClassName("box");
+	for (let i = 0; i < boxes.length; i++) {
+		boxes[i].style.zIndex = 1;
+	}
+}
+
+export { loadMap, fly };
